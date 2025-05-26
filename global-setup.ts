005dff05be35121ce6133f71ea58eb927f7e2557
Vinícius Carrier <vinicius.carrier@gmail.com>
2025-05-26 18:01:17 -0300

    Validação final

--- global-setup.ts
+++
@@ -1,6 +0,0 @@
-// global-setup.ts
-import * as dotenv from "dotenv";
-
-export default async () => {
-  dotenv.config();
-};
--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig, devices } from "@playwright/test";
+import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  testDir: "./tests",
+  testDir: './tests',
 
   //  Executar arquivos de teste em paralelo
   fullyParallel: true,
@@ -9,41 +9,42 @@ export default defineConfig({
   //  Impede que test.only fique por acidente
   forbidOnly: !!process.env.CI,
 
-  // Repetir testes no CI para evitar flakiness
+  //  Repetir testes no CI para evitar flakiness
   retries: process.env.CI ? 2 : 0,
 
-  // Se estiver no CI, rodar só com 1 worker para estabilidade
+  //  Se estiver no CI, rodar só com 1 worker para estabilidade
   workers: process.env.CI ? 1 : undefined,
 
   //  Relatório HTML gerado automaticamente
-  reporter: [["html", { open: "never" }]],
+  reporter: [['html', { open: 'never' }]],
 
   use: {
-  
-    // Coletar trace se falhar
-    trace: "on-first-retry",
 
-    // Capturar screenshot só se falhar
-    screenshot: "only-on-failure",
 
-    // Capturar vídeo só se falhar
-    video: "retain-on-failure",
+    //  Coletar trace se falhar
+    trace: 'on-first-retry',
+
+    //  Capturar screenshot só se falhar
+    screenshot: 'only-on-failure',
+
+    //  Capturar vídeo só se falhar
+    video: 'retain-on-failure',
   },
 
   projects: [
     {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
     },
     // Descomente se quiser também testar em outros navegadores:
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
   ],
 
   // Se tiver um dev server, configure aqui
